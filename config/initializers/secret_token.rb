ffd0cfdcd39e8a4630f905ba21e47f9220621700
Ed Schmalzle <ed@nerded.net>
2012-10-29 13:14:23 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Powerlapse::Application.config.secret_token = '1d89c4dd1c7c18b50240fa85df8ddd9359effadc43ac43d315bac639491e90d64ec743f991ee86479ad1baa8b9b16c98ba0c9831ae6a55dd8fff65a6d6fc6dd2'

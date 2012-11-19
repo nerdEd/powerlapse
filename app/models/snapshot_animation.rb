1a219a35a77a28067fdca46e34fb4a6e077d9d4e
Ed Schmalzle <ed@nerded.net>
2012-11-18 22:37:05 -0500

    add animation generation to snapshot animation

--- app/models/snapshot_animation.rb
+++ app/models/snapshot_animation.rb
@@ -1,8 +1,27 @@
+require 'open-uri'
+
 class SnapshotAnimation < ActiveRecord::Base
   attr_accessible :name
 
-  mount_uploader :snapshot, SnapshotAnimationUploader
+  mount_uploader :animation, SnapshotAnimationUploader
 
   belongs_to :event
   has_and_belongs_to_many :snapshots
+
+  validates :name, presence: true
+
+  def generate_animation
+    return if snapshots.empty?
+
+    imagelist = Magick::ImageList.new
+    snapshots.each do |snapshot|
+      imagelist.from_blob(open(snapshot.snapshot_url).read)
+    end
+
+    tmpfile = Tempfile.new('animation', Rails.root.join('tmp'))
+    imagelist.write(tmpfile)
+    animation = tmpfile
+
+    save!
+  end
 end
